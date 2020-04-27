e2e2c46199d69dc2dde6f0b64663e75f6b3e0945
Ricky <rickychea11@gmail.com>
2020-04-27 16:04:30 +0800

    - Added the email and password input type. - Added validation for email.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 var form = $(".needs-validation");
 form.submit(function(event) {
-  console.log("event: " +event);
   if(form[0].checkValidity() == false) {
-    console.log("false");
+    //console.log("false");
+    if($("input[name=email]").val() == "") {
+      $("small[id=email-warning]").text("Email can not be empty");
+    } else if(!validateEmail($("input[name=email]").val())) {
+      $("small[id=email-warning]").text("Please enter a valid email");
+    }
     event.preventDefault();
     event.stopPropagation();
     form.addClass("was-validated");
   }
 });
+
+
+function validateEmail(email) {
+  var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(String(email).toLowerCase());
+}
